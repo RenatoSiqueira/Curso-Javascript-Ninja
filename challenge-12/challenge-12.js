3c2006eded63c2e8b05413dd962d42a76240dfea
Renato Siqueira <renatoelysiqueira@gmail.com>
2018-08-02 00:50:05 -0300

    Challenge-12

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -83,32 +83,35 @@
     no formato abaixo:
         "[PROPRIEDADE]: [VALOR]"
     */
-    for ( var i = 0; i < Object.keys(books).length; i++) {
-        console.log( books[i].name, books[i].pages )
+    
+    for ( var i = 0; i < books.length; i++) {
+        for ( var prop in books[i] ) {
+            console.log( prop + ': ' + books[i][prop] );
+        }
     }
 
     /*
     Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
     seu nome. Adicione seu nome completo no array.
     */
-    var myName = [ 'r', 'e', 'n', 'a', 't', 'o', 's', 'i', 'q', 'u', 'e', 'i', 'r', 'a' ]
+    var myName = [ 'R', 'e', 'n', 'a', 't', 'o', 'S', 'i', 'q', 'u', 'e', 'i', 'r', 'a' ]
+
     console.log( '\nMeu nome é:' );
 
     /*
     Juntando todos os itens do array, mostre no console seu nome.
     */
-    console.log( myName.join('') )
+    console.log( myName.join('') );
 
     console.log( '\nMeu nome invertido é:' );
-
     /*
     Ainda usando o objeto acima, mostre no console seu nome invertido.
     */
-    console.log( myName.reverse().join('') )
+    console.log( myName.reverse().join('') );
 
     console.log( '\nAgora em ordem alfabética:' );
     /*
     Mostre todos os itens do array acima, odenados alfabéticamente.
     */
-    console.log( myName.sort() )
-})()
+    console.log( myName.sort() );
+})();
